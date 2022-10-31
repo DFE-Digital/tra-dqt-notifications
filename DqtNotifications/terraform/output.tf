c2ff5e3ae57a8e2652dd351d5fafb7324146ce0c
James Gunn <james@gunn.io>
2022-10-31 14:10:03 +0000

    Add build pipeline
    Added basic scaffolding for resource deployment
    
    Serivce Bus namespace
    
    update private-endpoint.tf
    
    Delete private-endpoint.tf
    
    code update
    
    update code
    
    codeupdate
    
    Added this branch to trigger
    
    update
    
    locals update
    
    variables update
    
    variables update
    
    app servcie plan kind update
    
    created postgress server and DB
    
    added postgress variables, etc
    
    Put comments for resources
    
    updated variables
    
    updated name
    
    updated container name
    
    container name and version
    
    update version
    
    storage account variable defined
    
    postgres server version changed to 11
    
    added var key vault
    
    updated
    
    updated
    
    updated
    
    updated
    
    adding providers config
    
    adding providers config
    
    update
    
    update statuscake
    
    update statuscake
    
    update statuscake
    
    update statuscake
    
    update statuscake
    
    updated data.tf
    
    updated output.tf
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    updated variables
    
    updated variables

---
+++ DqtNotifications/terraform/output.tf
@@ -0,0 +1,47 @@
+output "postgres_server_name" {
+  value = azurerm_postgresql_flexible_server.postgres-server.name
+}
+
+output "administrator_login" {
+  value = var.administrator_login
+}
+
+output "administrator_password" {
+  value     = var.administrator_password
+  sensitive = true
+}
+
+
+/*output "name" {
+  value = azurerm_servicebus_namespace.servicebus_namespace.name
+}
+
+# primary connection string for send and listen operations
+output "primary_send_and_listen_connection_string" {
+  value = azurerm_servicebus_namespace_authorization_rule.send_listen_auth_rule.primary_connection_string
+}
+
+# secondary connection string for send and listen operations
+output "secondary_send_and_listen_connection_string" {
+  value = azurerm_servicebus_namespace_authorization_rule.send_listen_auth_rule.secondary_connection_string
+}
+
+# primary shared access key with send and listen rights
+output "primary_send_and_listen_shared_access_key" {
+  value = azurerm_servicebus_namespace_authorization_rule.send_listen_auth_rule.primary_key
+}
+
+# secondary shared access key with send and listen rights
+output "secondary_send_and_listen_shared_access_key" {
+  value = azurerm_servicebus_namespace_authorization_rule.send_listen_auth_rule.secondary_key
+}
+
+# sku type
+output "sku" {
+  value = var.sku
+}
+
+output "id" {
+  value = azurerm_servicebus_namespace.servicebus_namespace.id
+}
+*/
