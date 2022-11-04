7a9d88e3fb22780f2fef5f4dd01f8582f1814ddb
enformatique11 <enformatique@hotmail.com>
2022-11-04 16:32:24 +0000

    status updatin

--- DqtNotifications/terraform/output.tf
+++ DqtNotifications/terraform/output.tf
@@ -1,4 +1,18 @@
-output "name" {
+output "postgres_server_name" {
+  value = azurerm_postgresql_flexible_server.postgres-server.name
+}
+
+output "administrator_login" {
+  value = var.administrator_login
+}
+
+output "administrator_password" {
+  value     = var.administrator_password
+  sensitive = true
+}
+
+
+/*output "name" {
   value = azurerm_servicebus_namespace.servicebus_namespace.name
 }
 
@@ -30,3 +44,4 @@ output "sku" {
 output "id" {
   value = azurerm_servicebus_namespace.servicebus_namespace.id
 }
+*/
