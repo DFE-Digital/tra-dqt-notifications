3737b6b1c2abc69b22bd34c70d479d456cdb1bca
enformatique11 <enformatique@hotmail.com>
2022-11-04 15:30:16 +0000

    update

--- DqtNotifications/terraform/variables.tf
+++ DqtNotifications/terraform/variables.tf
@@ -1,61 +1,28 @@
-variable "location" {
-  type    = string
-  default = "UK South"
-}
-
-variable "project" {
-  default     = ""
-  description = "Project name (required for Premium SKU) - sds or cft. "
-}
-
-variable "name" {
-  type        = string
-  description = "Unique Azure Service Bus namespace"
-}
 
 variable "resource_group_name" {
   type        = string
   description = "Resource group in which the Service Bus namespace should exist"
 }
 
-variable "env" {
+variable "environment_name" {
   type = string
 }
-
-variable "common_tags" {
-  type = map(string)
-}
-
 variable "sku" {
   type        = string
   default     = "Standard"
   description = "SKU type (Basic, Standard and Premium)"
 }
 
-variable "capacity" {
-  type        = number
-  default     = 0
-  description = "Specifies the capacity. Defaults to 1 when using Premium SKU."
+variable "resource_prefix" {
+  type    = string
+  default = ""
 }
 
-variable "zone_redundant" {
-  type        = bool
-  default     = false
-  description = "Whether or not this resource is zone redundant (true or false)"
+variable "app_suffix" {
+  type    = string
+  default = ""
 }
-
-variable "enable_public_access" {
-  type        = bool
-  default     = false
-  description = "Enable public access (should only be enabled for a migration when using the Premium SKU and a private endpoint connection)"
-}
-
-variable "enable_private_endpoint" {
-  default     = false
-  description = "Enable Private endpoint? Only available with the Premium SKU, if set to true a Premium type Service Bus Namespace will be deployed automatically"
-}
-
-variable "subnet_id" {
-  default     = ""
-  description = "Subnet ID to attach private endpoint to - overrides the default subnet id"
+locals {
+  hosting_environment = var.environment_name
+  service_bus_name    = "${var.resource_prefix}rsm-${var.environment_name}${var.app_suffix}-srbsnsp"
 }
