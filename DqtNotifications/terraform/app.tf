5636c4b3c5857d57788c92e6e6a588bdd13c3fab
enformatique11 <enformatique@hotmail.com>
2022-11-07 14:04:51 +0000

    Put comments for resources

--- DqtNotifications/terraform/app.tf
+++ DqtNotifications/terraform/app.tf
@@ -4,74 +4,8 @@ locals {
   capacity       = local.sku == "Premium" && var.capacity <= 0 ? 1 : var.capacity
 }
 
-resource "azurerm_servicebus_namespace" "servicebus_namespace" {
-  name                = local.servicebus_namespace_name
-  location            = data.azurerm_resource_group.rgsb.location
-  resource_group_name = data.azurerm_resource_group.rgsb.name
-  sku                 = local.sku
-  zone_redundant      = var.zone_redundant
-  capacity            = local.capacity
 
-  lifecycle {
-    ignore_changes = [
-      tags
-    ]
-  }
-}
-
-resource "azurerm_servicebus_namespace_authorization_rule" "send_listen_auth_rule" {
-  name         = local.auth_rule_name
-  namespace_id = azurerm_servicebus_namespace.servicebus_namespace.id
-
-  listen = true
-  send   = true
-
-
-}
-
-
-resource "azurerm_servicebus_topic" "servicebus_topic" {
-  name         = local.servicebus_topic_name
-  namespace_id = azurerm_servicebus_namespace.servicebus_namespace.id
-
-  enable_partitioning = true
-
-
-}
-
-# App service plan
-resource "azurerm_storage_account" "storage_account" {
-  name                              = local.storage_account_name
-  resource_group_name               = data.azurerm_resource_group.rgsb.name
-  location                          = data.azurerm_resource_group.rgsb.location
-  account_tier                      = "Standard"
-  account_replication_type          = var.environment_name != "dev" ? "LRS" : "GRS"
-  account_kind                      = "StorageV2"
-  min_tls_version                   = "TLS1_2"
-  infrastructure_encryption_enabled = true
-
-  blob_properties {
-    last_access_time_enabled = true
-
-    container_delete_retention_policy {
-      days = var.data_protection_container_delete_retention_days
-    }
-  }
-
-  lifecycle {
-    ignore_changes = [
-      tags
-    ]
-  }
-
-}
-
-resource "azurerm_storage_container" "keys" {
-  name                  = local.storage_container_name
-  storage_account_name  = azurerm_storage_account.storage_account.name
-  container_access_type = "private"
-}
-
+# App Sercie Plan
 resource "azurerm_app_service_plan" "app_service_plan" {
   name                = local.app_service_plan_name
   location            = data.azurerm_resource_group.rgsb.location
@@ -92,6 +26,8 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   }
 }
 
+
+# Function App
 resource "azurerm_function_app" "functionAp" {
   name                       = local.azurerm_function_app_name
   location                   = data.azurerm_resource_group.rgsb.location
@@ -106,6 +42,8 @@ resource "azurerm_function_app" "functionAp" {
     ]
   }
 }
+
+# Postgress Server
 
 resource "azurerm_postgresql_flexible_server" "postgres-server" {
   name                   = local.postgres_server_name
@@ -134,7 +72,87 @@ resource "azurerm_postgresql_flexible_server" "postgres-server" {
   }
 }
 
+# database
+
 resource "azurerm_postgresql_flexible_server_database" "postgres-database" {
   name      = local.postgres_database_name
   server_id = azurerm_postgresql_flexible_server.postgres-server.id
 }
+
+# Servicebus Namespace
+
+resource "azurerm_servicebus_namespace" "servicebus_namespace" {
+  name                = local.servicebus_namespace_name
+  location            = data.azurerm_resource_group.rgsb.location
+  resource_group_name = data.azurerm_resource_group.rgsb.name
+  sku                 = local.sku
+  zone_redundant      = var.zone_redundant
+  capacity            = local.capacity
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+}
+
+# Servicebus Auth Rule
+
+resource "azurerm_servicebus_namespace_authorization_rule" "send_listen_auth_rule" {
+  name         = local.auth_rule_name
+  namespace_id = azurerm_servicebus_namespace.servicebus_namespace.id
+
+  listen = true
+  send   = true
+
+
+}
+
+# Servicebus Topic
+
+resource "azurerm_servicebus_topic" "servicebus_topic" {
+  name         = local.servicebus_topic_name
+  namespace_id = azurerm_servicebus_namespace.servicebus_namespace.id
+
+  enable_partitioning = true
+
+
+}
+
+#Storage account
+resource "azurerm_storage_account" "storage_account" {
+  name                              = local.storage_account_name
+  resource_group_name               = data.azurerm_resource_group.rgsb.name
+  location                          = data.azurerm_resource_group.rgsb.location
+  account_tier                      = "Standard"
+  account_replication_type          = var.environment_name != "dev" ? "LRS" : "GRS"
+  account_kind                      = "StorageV2"
+  min_tls_version                   = "TLS1_2"
+  infrastructure_encryption_enabled = true
+
+  blob_properties {
+    last_access_time_enabled = true
+
+    container_delete_retention_policy {
+      days = var.data_protection_container_delete_retention_days
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+
+}
+
+#Storage container
+resource "azurerm_storage_container" "keys" {
+  name                  = local.storage_container_name
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
+
+
+
+
