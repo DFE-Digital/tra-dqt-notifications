b850cf2e5c260d153331aa3596b5ce1656644c30
enformatique11 <enformatique@hotmail.com>
2022-11-08 10:16:53 +0000

    update statuscake

--- DqtNotifications/terraform/providers.tf
+++ DqtNotifications/terraform/providers.tf
@@ -1,9 +1,4 @@
-/*locals {
-  azure_credentials      = try(jsondecode(var.azure_sp_credentials_json), null)
-  infrastructure_secrets = yamldecode(data.azurerm_key_vault_secret.secrets["INFRASTRUCTURE"].value)
-  monitoring_secrets     = yamldecode(data.azurerm_key_vault_secret.secrets["MONITORING"].value)
-}
-*/
+
 provider "azurerm" {
   subscription_id            = try(local.azure_credentials.subscriptionId, null)
   client_id                  = try(local.azure_credentials.clientId, null)
