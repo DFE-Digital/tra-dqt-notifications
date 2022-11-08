01fbea51a74ea3946d556a3fc8efec8eb3f85fa4
enformatique11 <enformatique@hotmail.com>
2022-11-08 11:31:45 +0000

    update

--- DqtNotifications/terraform/providers.tf
+++ DqtNotifications/terraform/providers.tf
@@ -4,7 +4,7 @@ locals {
   azure_credentials      = try(jsondecode(var.azure_sp_credentials_json), null)
   infrastructure_secrets = yamldecode(data.azurerm_key_vault_secret.secrets["INFRASTRUCTURE"].value)
 
-  
+  }
 provider "azurerm" {
   subscription_id            = try(local.azure_credentials.subscriptionId, null)
   client_id                  = try(local.azure_credentials.clientId, null)
