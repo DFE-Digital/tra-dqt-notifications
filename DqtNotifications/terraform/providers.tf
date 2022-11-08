66f36724ee3c82d0c35ab0bdf703e829dff5a004
enformatique11 <enformatique@hotmail.com>
2022-11-08 10:30:08 +0000

    update statuscake

--- DqtNotifications/terraform/providers.tf
+++ DqtNotifications/terraform/providers.tf
@@ -1,14 +1,1 @@
 
-provider "azurerm" {
-  subscription_id            = try(local.azure_credentials.subscriptionId, null)
-  client_id                  = try(local.azure_credentials.clientId, null)
-  client_secret              = try(local.azure_credentials.clientSecret, null)
-  tenant_id                  = try(local.azure_credentials.tenantId, null)
-  skip_provider_registration = true
-
-  features {}
-}
-
-#provider "statuscake" {
- # api_token = local.monitoring_secrets.STATUSCAKE_PASSWORD
-#}
