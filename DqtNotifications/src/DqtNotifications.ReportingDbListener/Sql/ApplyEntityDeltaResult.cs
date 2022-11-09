540a5a2476b821cfedb5257f1e5b8d979714ce58
James Gunn <james@gunn.io>
2022-11-09 15:59:15 +0000

    Add support for deleting rows

--- DqtNotifications/src/DqtNotifications.ReportingDbListener/Sql/ApplyEntityDeltaResult.cs
+++ DqtNotifications/src/DqtNotifications.ReportingDbListener/Sql/ApplyEntityDeltaResult.cs
@@ -3,6 +3,5 @@ namespace DqtNotifications.ReportingDbListener.Sql;
 public enum ApplyEntityDeltaResult
 {
     Success = 0,
-    StaleDataError = 1,
-    RowDoesNotExist = 2
+    StaleDataError = 1
 }
