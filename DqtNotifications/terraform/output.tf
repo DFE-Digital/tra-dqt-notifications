37d6794e0173ec256cfdfe1625ea3f2ab10ca945
enformatique11 <enformatique@hotmail.com>
2022-11-09 16:15:29 +0000

    deploy

--- DqtNotifications/terraform/output.tf
+++ DqtNotifications/terraform/output.tf
@@ -16,10 +16,7 @@ output "administrator_password" {
   value = azurerm_servicebus_namespace.servicebus_namespace.name
 }
 
-# primary connection string for send and listen operations
-output "primary_send_and_listen_connection_string" {
-  value = azurerm_servicebus_namespace_authorization_rule.send_listen_auth_rule.primary_connection_string
-}
+
 
 # secondary connection string for send and listen operations
 output "secondary_send_and_listen_connection_string" {
