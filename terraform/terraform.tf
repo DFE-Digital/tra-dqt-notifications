64cc44d95955e12d33a1805ad9d182144b9b1c87
enformatique11 <enformatique@hotmail.com>
2022-11-12 12:16:02 +0000

    updates

---
+++ terraform/terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.0"
+
+  backend "azurerm" {
+    container_name = "dqtnoti-tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.84"
+    }
+  }
+}
